063ba1db8aa007192056da96369550b315b64882
mahathikamal <mahathikb@gmail.com>
2026-02-20 01:54:01 +0530

    Create Product.java

---
+++ src/Product.java
@@ -0,0 +1,30 @@
+public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+    public Product(int id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public double getTotalValue() {
+        return quantity * price;
+    }
+    public void display() {
+        System.out.printIn(id + "\t" + name + "\t" + quantity + "\t" + price);
+    }
+}
