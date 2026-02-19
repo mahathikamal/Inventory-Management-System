f446912dc5bdc6cb5d2113534a45e2e70c5b6c42
Bhanu <Bhanu@MAHATHI>
2026-02-19 22:15:11 +0530

    first commit

--- src/inventory/Product.java
+++ src/inventory/Product.java
@@ -1,16 +1,5 @@
 package inventory;
 
 public class Product {
-	private int id;
-	private String name;
-	private int quantity;
-	private double price;
-public Product(int id, String name, int quantity, double price) {
-	this.id = id;
-	this.name = name;
-	this.quantity = quantity;
-	this.price = price;
-}
-
 
 }
